a3374145e0906c138cb1c8aaddce6312f156bc4a
“G-Gakii” <gakiiviolet1@gmail.com>
2024-10-25 11:42:05 +0300

    package json setting

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello ,Typescript wuth express ");
+    res.send("Hello ,Typescript with express ");
 });
 app.listen(PORT, () => {
     console.log(`listening at port ${PORT} `);
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello ,Typescript with express ");
+  res.send("Hello ,Typescript with express.hey ");
 });
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT} `);
