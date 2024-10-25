fd8f8ccf58ce7a81c99a64c6827de976757d3594
“G-Gakii” <gakiiviolet1@gmail.com>
2024-10-25 11:30:38 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello ,Typescript with express ");
+});
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT} `);
+});
